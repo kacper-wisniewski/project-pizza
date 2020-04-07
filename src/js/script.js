10cece212e1a825c835d969476cf2ba3fc15d3fe
Kacper Wiśniewski <kaka1341@gmail.com>
2020-04-07 11:52:28 +0200

    Add cart - WIP

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,7 @@
   const select = {
     templateOf: {
       menuProduct: '#template-menu-product',
+      cartProduct: '#template-cart-product', // CODE ADDED
     },
     containerOf: {
       menu: '#product-list',
@@ -26,38 +27,98 @@
     },
     widgets: {
       amount: {
-        input: 'input[name="amount"]',
+        input: 'input.amount', // CODE CHANGED
         linkDecrease: 'a[href="#less"]',
         linkIncrease: 'a[href="#more"]',
       },
     },
+    // CODE ADDED START
+    cart: {
+      productList: '.cart__order-summary',
+      toggleTrigger: '.cart__summary',
+      totalNumber: `.cart__total-number`,
+      totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+      subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+      deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+      form: '.cart__order',
+      formSubmit: '.cart__order [type="submit"]',
+      phone: '[name="phone"]',
+      address: '[name="address"]',
+    },
+    cartProduct: {
+      amountWidget: '.widget-amount',
+      price: '.cart__product-price',
+      edit: '[href="#edit"]',
+      remove: '[href="#remove"]',
+    },
+    // CODE ADDED END
   };
-
+  
   const classNames = {
     menuProduct: {
       wrapperActive: 'active',
       imageVisible: 'active',
     },
+    // CODE ADDED START
+    cart: {
+      wrapperActive: 'active',
+    },
+    // CODE ADDED END
   };
-
+  
   const settings = {
     amountWidget: {
       defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9,
-    }
+    }, // CODE CHANGED
+    // CODE ADDED START
+    cart: {
+      defaultDeliveryFee: 20,
+    },
+    // CODE ADDED END
   };
-
+  
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+    // CODE ADDED START
+    cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+    // CODE ADDED END
   };
+
+  class CartProduct {
+
+  }
+
+  class Cart {
+    constructor(element) {
+      const thisCart = this;
+      thisCart.products = [];
+      thisCart.getElements(element);
+      thisCart.initActions();
+      console.log('New Cart:', thisCart);
+    }
+    getElements(element) {
+      const thisCart = this;
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = document.querySelector(select.cart.toggleTrigger);
+    }
+    initActions() {
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function() {
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
 
   class AmountWidget {
     constructor(element) {
       const thisWidget = this;
       thisWidget.getElements(element);
       thisWidget.initActions();
-      thisWidget.value = settings.amountWidget[defaultValue];
+      thisWidget.value = settings.amountWidget['defaultValue'];
       thisWidget.setValue(thisWidget.input.value);
     }
     getElements(element) {
@@ -86,12 +147,12 @@
     setValue(value) {
       const thisWidget = this;
       const newValue = parseInt(value);
-      if(newValue >= settings.amountWidget[defaultMin] && newValue <= settings.amountWidget[defaultMax]) {
+      if(newValue >= settings.amountWidget['defaultMin'] && newValue <= settings.amountWidget['defaultMax']) {
         thisWidget.value = newValue;
-      } else if(newValue > settings.amountWidget[defaultMax]) {
-        thisWidget.value = settings.amountWidget[defaultMax];
-      } else if(newValue < settings.amountWidget[defaultMin]) {
-        thisWidget.value = settings.amountWidget[defaultMin];
+      } else if(newValue > settings.amountWidget['defaultMax']) {
+        thisWidget.value = settings.amountWidget['defaultMax'];
+      } else if(newValue < settings.amountWidget['defaultMin']) {
+        thisWidget.value = settings.amountWidget['defaultMin'];
       }
       thisWidget.announce();
       thisWidget.input.value = thisWidget.value;
@@ -240,6 +301,12 @@
 
       thisApp.data = dataSource;
     },
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -248,8 +315,9 @@
       console.log('settings:', settings);
       console.log('templates:', templates);
       thisApp.initData();
-      app.initMenu();
-    },
+      thisApp.initMenu();
+      thisApp.initCart();
+    }
   };
 
   
