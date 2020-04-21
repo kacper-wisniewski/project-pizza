705089e50727ecf12b3697a62b5cfd669d21b007
Kacper Wiśniewski <kaka1341@gmail.com>
2020-04-21 17:55:56 +0200

    Add initPages & activatedPage methods

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,75 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function() {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = false;
+
+    for(let page of thisApp.pages) {
+      if(page.id == idFromHash) {
+        pageMatchingHash = true;
+        //pageMatchingHash = idFromHash;
+        break;
+      }
+      /* else {
+        pageMatchingHash = thisApp.pages[0].id;
+      } */
+    }
+
+    //thisApp.activePage(pageMatchingHash);
+
+    if(pageMatchingHash) {
+      thisApp.activatePage(idFromHash);
+    } else {
+      thisApp.activatePage(thisApp.pages[0].id);
+    }
+
+    for(let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event) {
+        const clickedElement = this;
+        event.preventDefault();
+        
+        const pageId = clickedElement.getAttribute('href').replace('#', '');
+        
+        //const pageId = href.replace('#', '');
+        //console.log(pageId);
+        
+        thisApp.activatePage(pageId);
+
+        /* change URL hash */
+        window.location.hash = `#/${pageId}`
+      });
+    }
+  },
+  activatePage: function(pageId) {
+    const thisApp = this;
+    
+    /* add class 'active' to matching pages, remove from non-matching */
+  
+    /* add class 'active' to matching link, remove from non-matching */
+
+    for(let page of thisApp.pages) {
+      /* if(page.id == pageId) {
+        page.classList.add(classNames.pages.active);
+      } else {
+        page.classList.remove(classNames.pages.active);
+      } */
+      page.classList.toggle(classNames.pages.active, page.id == pageId);  
+    }
+    for(let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == `#${pageId}`
+      );
+    }
+  },
   initMenu: function() {
     const thisApp = this;
     for(let productData in thisApp.data.products) {
@@ -26,7 +93,7 @@ const app = {
 
         app.initMenu();
       });
-    console.log('thisApp.data', JSON.stringify(thisApp.data)); 
+    //console.log('thisApp.data', JSON.stringify(thisApp.data)); 
   },
   initCart: function(){
     const thisApp = this;
@@ -45,6 +112,8 @@ const app = {
     console.log('classNames:', classNames);
     console.log('settings:', settings);
     console.log('templates:', templates);*/
+    
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   }
