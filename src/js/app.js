d087942a47a5d05ac64eeb81e7e94b16ba006da3
Kacper Wiśniewski <kaka1341@gmail.com>
2020-04-21 18:55:19 +0200

    Add class Booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initPages: function() {
@@ -44,7 +45,8 @@ const app = {
         thisApp.activatePage(pageId);
 
         /* change URL hash */
-        window.location.hash = `#/${pageId}`
+      
+        window.location.hash = `#/${pageId}`;
       });
     }
   },
@@ -105,6 +107,12 @@ const app = {
       app.cart.add(event.detail.product);
     });
   },
+  initBooking: function() {
+    const thisApp = this;
+    const bookingElem = document.querySelector(select.containerOf.booking);
+
+    thisApp.booking = new Booking(bookingElem);
+  },
   init: function(){
     const thisApp = this;
     /*console.log('*** App starting ***');
@@ -116,6 +124,7 @@ const app = {
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   }
 };
 
