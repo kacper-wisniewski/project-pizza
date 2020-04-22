379d5a8d30b91eeaa00a793c125278872a21911e
Kacper Wiśniewski <kaka1341@gmail.com>
2020-04-22 18:14:37 +0200

    Fix HourPicker

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -8,15 +8,19 @@ class HourPicker extends BaseWidget {
     const thisWidget = this;
     thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.input);
     thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
+    
+    thisWidget.value = thisWidget.dom.input.value;
+    thisWidget.renderValue();
     thisWidget.initPlugin();
-    thisWidget.dom.input.addEventListener('change', function() {
-      thisWidget.renderValue();
-    });
   }
   initPlugin() {
     const thisWidget = this;
     console.log(thisWidget);
     rangeSlider.create(thisWidget.dom.input);
+    thisWidget.dom.input.addEventListener('change', function() {
+      thisWidget.value = thisWidget.dom.input.value;
+      thisWidget.renderValue();
+    })
   }
   parseValue(value) {
     return utils.numberToHour(value);
@@ -26,7 +30,7 @@ class HourPicker extends BaseWidget {
   }
   renderValue() {
     const thisWidget = this;
-    thisWidget.dom.output = thisWidget.value;
+    thisWidget.dom.output.innerHTML = thisWidget.value;
   }
 }
 export default HourPicker;
